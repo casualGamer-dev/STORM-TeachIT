069f5499ddd56699f665908966e371748a984b69
casualGamer-dev <60314853+casualGamer-dev@users.noreply.github.com>
2025-03-16 18:42:28 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -16,15 +16,16 @@ import {
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAGMLXExaLCwMQ4W-k-Csl4mViyZXghgHM",
-  authDomain: "sincere-etching-356016.firebaseapp.com",
-  databaseURL: "https://sincere-etching-356016-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "sincere-etching-356016",
-  storageBucket: "sincere-etching-356016.appspot.com",
-  messagingSenderId: "1011466224742",
-  appId: "1:1011466224742:web:6ce1cf0b3c4c8d25b599a7",
-  measurementId: "G-5DBZ4BKW34"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain:   process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase services
 const app = initializeApp(firebaseConfig);
